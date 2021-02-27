589978234d8a8f26270322d8cf669e44325bd32e
aidan-lane <aidanlane7@gmail.com>
2021-02-27 00:03:07 -0500

    fixed test error

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,12 +3,15 @@
     <v-card class="s-card">
       <v-col cols="12">
         <h1>Your Settings</h1>
-        <v-row>
+        <v-row class="sex-row" justify="space-around">
           <v-col cols="6">
             <v-text-field rounded solo> </v-text-field>
           </v-col>
+          <v-col cols="6">
+            <v-select outlined></v-select>
+          </v-col>
         </v-row>
-        <v-row>
+        <v-row class="weight-row" justify="space-around">
           <v-col cols="6">
             <v-text-field rounded solo> </v-text-field>
           </v-col>
@@ -25,5 +28,12 @@ export default {};
 <style>
 .s-card {
   padding: 5% 5% 5% 5%;
+}
+
+.sex-row {
+  margin-bottom: -2.5em;
+}
+
+.weight-row {
 }
 </style>
